34f650ed23f009f5e0a3a0289566dd895498189b
Aodan O'Sullivan <aodanosullivan@gmail.com>
2019-01-24 20:40:40 +0100

    added filter feature for tags and ingredients only when assigned

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -92,3 +92,23 @@ class PrivateIngredientAPITests(TestCase):
         res = self.client.post(INGREDIENTS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_ingredients_assigned_to_recipes(self):
+        """Test filtering ingredients by those assigned to recipes"""
+        ingredient1 = Ingredient.objects.create(user=self.user, name="Onion")
+        ingredient2 = Ingredient.objects.create(user=self.user, name="Beef")
+
+        recipe = Recipe.objects.create(
+            user=self.user,
+            title="Lasagna",
+            price=5.00,
+            time_minutes=12
+        )
+        recipe.ingredients.add(ingredient1)
+
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
+        serializer1 = IngredientSerializer(ingredient1)
+        serializer2 = IngredientSerializer(ingredient2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
